da4f5d45dc318f04c4d9c50841b04e260931d298
luningcowboy <luningcowboy@gmail.com>
2019-06-06 21:05:07 +0800

    new sort

---
+++ sort-js/countingSort.js
@@ -0,0 +1,23 @@
+function countingSort(arr, maxValue){
+    let bucket = new Array(maxValue - 1),
+        sortedIndex = 0,
+        arrLen = arr.length,
+        bucketLen = maxValue + 1;
+    for(let i = 0; i < arrLen; i++){
+        if(!bucket[arr[i]]){
+            bucket[arr[i]] = 0;
+        }
+        bucket[arr[i]]++;
+    }
+    for(let j = 0; j < bucketLen; j++){
+        while(bucket[j] > 0){
+            arr[sortedIndex++] = j;
+            bucket[j]--;
+        }
+    }
+    return arr;
+}
+let arr = [1,4,3,2,6,7,9,5];
+console.log("before sort:", arr);
+countingSort(arr, arr.length);
+console.log("after sort:", arr);
---
+++ sort-js/radixSort.js
@@ -0,0 +1,22 @@
+let counter = [];
+function radixSort(arr, maxDigit){
+    let mod = 10;
+    let dev = 1;
+    for(let i = 0; i < maxDigit; i++, dev *= 10, mod *= 10){
+        for(let j = 0; j < arr.length; j++){
+            let bucket = parseInt((arr[j] % mode) / dev);
+            if(counter[bucket] === null) counter[bucket] = [];
+            counter[bucket].push(arr[j]);
+        }
+        let pos = 0;
+        for(let j = 0; j < counter.length; j++){
+            let value = null;
+            if(counter[j] !== null){
+                while((value = counter[j].shift()) !== null){
+                    arr[pos++] = value;
+                }
+            }
+        }
+    }
+    return arr;
+}
