f1af268509293decb7e966d6c97a0010e72a0f8e
luningcowboy <luningcowboy@gmail.com>
2019-12-30 14:52:29 +0800

    new code

---
+++ c/ch2/yunsuan.c
@@ -0,0 +1,9 @@
+// 添加 + - x / 4个运算符使等式 5 5 5 5 5=5 成立
+// x / 优先级高于 + -
+#include<stdio.h>
+int main(){
+    int operate = {1,2,3,4};
+    int nums = {5,5,5,5,5};
+    int result = 5;
+    return 0;
+}
