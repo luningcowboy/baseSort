1a2b3175ffe2012f205325b7ebdb0cc63cdc09bd
luningcowboy <luningcowboy@gmail.com>
2020-01-07 13:16:52 +0800

    add new code

--- grokking_algorithms/ch1/cpp11/main.cpp
+++ grokking_algorithms/ch1/cpp11/main.cpp
@@ -20,9 +20,29 @@ int binary_search(const std::vector<T>& list, const int& item)
     }
     return -1;
 }
+template <typename T>
+const T* binary_search2(const std::vector<T>& list, const T& item)
+{
+    const T* low = &list.front();
+    const T* high = &list.back();
+
+    while (low <= high) {
+        const T* guess = low + ((high - low) / 2);
+        if (*guess == item)
+            return guess;
+        if (*guess > item)
+            high = guess - 1;
+        else
+            low = guess + 1;
+    }
+    return nullptr;
+}
 int main()
 {
     std::vector<int> my_list = { 1, 2, 3, 4, 5 };
     cout << binary_search(my_list, 3) << endl;
+    const int* ret = binary_search2(my_list, 4);
+    cout << *ret << endl;
+    cout << ret << endl;
     return 0;
 }
