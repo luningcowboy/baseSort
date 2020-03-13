9f6f1ea76b9b69042904972fa41bb7dd86ba7604
luningcowboy <luningcowboy@gmail.com>
2020-03-13 09:31:27 +0800

    add new code

--- c/arr/arr_0.c
+++ c/arr/arr_0.c
@@ -18,6 +18,51 @@ table initTable(){
     t.size = SIZE;
     return t;
 }
+table addTable(table t, int elem, int add){
+    if(add > t.length + 1 || add < 1){
+        printf("addTable error 1");
+        return t;
+    }
+    if(t.length >= t.size){
+        t.head = (int *)realloc(t.head, (t.size + 1)*sizeof(int));
+        if(!t.head){
+            printf("addTable error 2");
+        }
+        t.size += 1;
+    }
+    for(int i = t.length - 1; i >= add - 1; i--){
+        t.head[i + 1] = t.head[i];
+    }
+    t.head[add-1] = elem;
+    t.length++;
+    return t;
+}
+table delTable(table t, int add){
+    if (add > t.length || add < 1){
+        printf("delTable: 被删除的元素位置有误");
+        exit(0);
+    }
+    for(int i = add; i < t.length; i++){
+        t.head[i - 1] = t.head[i];
+    }
+    t.length--;
+    return t;
+}
+int selectTable(table t, int elem){
+    for(int i = 0; i < t.length; i++){
+        if(t.head[i] == elem){
+            return i + 1;
+        }
+    }
+    return -1;
+}
+table amendTable(table t, int elem, int newElem){
+    int add = selectTable(t, elem);
+    if(add != -1){
+        t.head[add - 1] = newElem;
+    }
+    return t;
+}
 void displayTable(table t){
     for(int i = 0; i < t.length; i++){
         printf("%d ", t.head[i]);
@@ -30,6 +75,18 @@ int main(){
         t.head[i - 1] = i;
         t.length++;
     }
+    displayTable(t);
+
+    t = delTable(t, 1);
+    displayTable(t);
+
+    t = addTable(t, 5, 2);
+    displayTable(t);
+
+    int add = selectTable(t, 3);
+    printf("%d", add);
+
+    t = amendTable(t, 3, 6);
     displayTable(t);
     return 0;
 }
